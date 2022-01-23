9d9771677ed20b77df919285ca55e0c5c8777176
nsym <mickey-ya@outlook.jp>
2022-01-23 21:14:58 +0900

    add: ルーティング追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::get('/register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user/profile', [\App\Http\Controllers\UserController::class, 'index'])->name('profile');
+    Route::get('/user/update', [\App\Http\Controllers\UserController::class, 'update'])->name('profileUpdate');
+    Route::get('/comment', [\App\Http\Controllers\CommentController::class, 'onlyUserTimeline'])->name('onlyUserTimeline');
+    Route::post('/comment', [\App\Http\Controllers\CommentController::class, 'post'])->name('post');
+    Route::get('/comment/all', [\App\Http\Controllers\CommentController::class, 'allUserTimeline'])->name('allUserTimeline');
 });
