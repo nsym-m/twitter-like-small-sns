5d946298d45addd3bb50a9bf88cade2c9c1dcbe9
nsym <mickey-ya@outlook.jp>
2022-01-23 15:14:45 +0900

    update: migrateionの設定

--- backend/database/migrations/2022_01_23_055934_create_comments_table.php
+++ backend/database/migrations/2022_01_23_055934_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('user_code', 100)->unique();;
-            $table->string('user_name', 100);
-            $table->string('password');
-            $table->string('profile_image', 50);
+            $table->unsignedBigInteger('user_id');
+            $table->string('content', 140);
+            $table->string('image', 50);
             $table->timestamps();
             $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
