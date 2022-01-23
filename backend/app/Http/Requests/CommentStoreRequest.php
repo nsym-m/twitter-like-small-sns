72dfec9e4467afda59bee6905545cc2b0379ec7a
nsym <mickey-ya@outlook.jp>
2022-01-23 20:29:58 +0900

    add: Requestを仮追加

---
+++ backend/app/Http/Requests/CommentStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CommentStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|string|max:100',
+            'content' => 'required|string|max:140',
+            'image' => 'required|mimes:jpg,bmp,png',
+        ];
+    }
+}
