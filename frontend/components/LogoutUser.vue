b6eee622a43575085959f49468d99ea6ddd6cfce
nsym <mickey-ya@outlook.jp>
2022-01-24 12:29:39 +0900

    add:  ログアウト画面仮追加

---
+++ frontend/components/LogoutUser.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn color="blue" text @click="logout">logout</v-btn>
+</template>
+
+<script>
+export default {
+  middleware ({ store, redirect }) {
+    if (!store.$auth.loggedIn) {
+      redirect('/login')
+    }
+  },
+  computed: {
+    user: () => {
+      return this.$auth.user
+    }
+  },
+  methods: {
+    logout: () => {
+      this.$auth.logout()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 50px auto;
+  width: 300px;
+  text-align: center;
+
+  .button {
+    margin-top: 30px;
+  }
+}
+</style>
