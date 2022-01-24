83adb6c9013ffbf3d885dcf9dc4034f9b21236f0
nsym <mickey-ya@outlook.jp>
2022-01-25 03:08:02 +0900

    add: プロフィール用のコンポーネント作成

---
+++ frontend/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-avatar size="80" class="mb-3 avatar">
+      <img
+        src="`{{user.profile_image}}`"
+        alt="avatarImage"
+        class="avatar-image"
+      >
+    </v-avatar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    user: {}
+  })
+}
+</script>
