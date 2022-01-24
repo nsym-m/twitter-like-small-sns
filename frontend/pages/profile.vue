451c9edd4e005a736e4b63249f6f0b2d9803a358
nsym <mickey-ya@outlook.jp>
2022-01-24 12:32:03 +0900

    add: ログアウト画面(プロフィール詳細にもする)追加

---
+++ frontend/pages/profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <LogoutUser />
+</template>
+
+<script>
+import LogoutUser from '@/components/LogoutUser.vue'
+
+export default {
+  components: LogoutUser,
+  data () {
+    return {
+      dialog: false,
+      rssForm: {
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addFeed: () => {
+      // // 記事を追加
+      // const res1 = await this.$axios.$post('/api/v1/feeds', {
+      //   feed: this.rssForm
+      // })
+      // this.dialog = false
+      // this.rssForm = ''
+    }
+  }
+}
+</script>
