74d7761827e7e6c0d21760df8e9235d466004ae6
nsym <mickey-ya@outlook.jp>
2022-01-25 03:08:27 +0900

    add: タイムライン用のコンポーネント作成

---
+++ frontend/components/TimeLine.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-list>
+    <template v-for="(comment, index) in comments">
+      <v-divider :key="`div-${index}`" />
+      <v-list-item :key="index">
+        <v-list-item-avatar>
+          <v-img :src="`{{ comment.profile_image }}`" size="40" />
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ comment.content }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+  </v-list>
+</template>
+
+<script>
+export default {
+  props: {
+    comments: {
+      type: Array,
+      required: true
+    }
+  }
+  // data: () => ({
+  //   questionTypes: QreConstants.questionTypes,
+  //   modal_dismiss: null
+  // })
+
+}
+</script>
